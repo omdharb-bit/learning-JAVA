fc207304e053d0ea97a4935833bda92c7c05b504
Om Dhar Badgaiyan <omdharb@gmail.com>
2025-11-21 16:18:01 +0530

    hii

--- interface/src/Main.java
+++ interface/src/Main.java
@@ -15,64 +15,65 @@
 //        s1.getb();
 //        s1.setBalance(500);
 //    }
-// }
+// }import java.util.Scanner;
+//
+//import java.util.Scanner;
+import java.util.Scanner;
 
 class Youtube {
-private String ChannelName;
-private int SubscriberCount;
+    private String ChannelName;
+    private int SubscriberCount;
+    private int pin;   // PIN for security
 
-public Youtube(String ChannelName, int SubscriberCount) {
-    this.ChannelName = ChannelName;
-    this.SubscriberCount = SubscriberCount;
-}
-
-public void getChannelName() {
-    System.out.println("Channel Name: " + ChannelName);
-}
-
-public void getSubscriberCount() {
-    System.out.println("Subscriber Count: " + SubscriberCount);
-}
-
-public void increaseSubscriberCount() {
-    SubscriberCount++;
-}
-
-public void decreaseSubscriberCount() {
-    if (SubscriberCount > 0) {
-        SubscriberCount--;
-    } else {
-        System.out.println("Error: No subscribers to remove!");
+    public Youtube(String ChannelName, int SubscriberCount, int pin) {
+        this.ChannelName = ChannelName;
+        this.SubscriberCount = SubscriberCount;
+        this.pin = pin;
     }
-}
+
+    // Method to verify PIN before showing subscriber count
+    public void getSubscriberCount(int enteredPin) {
+        if (enteredPin == pin) {
+            System.out.println("Subscriber Count: " + SubscriberCount);
+        } else {
+            System.out.println("Incorrect PIN! Access Denied ❌");
+        }
+    }
+
+    public void getChannelName() {
+        System.out.println("Channel Name: " + ChannelName);
+    }
+
+    public void increaseSubscriberCount() {
+        SubscriberCount++;
+    }
+
+    public void decreaseSubscriberCount() {
+        if (SubscriberCount > 0) {
+            SubscriberCount--;
+        } else {
+            System.out.println("Error: No subscribers to remove!");
+        }
+    }
 }
 
 public class Main {
     public static void main(String[] args) {
-        Youtube y1 = new Youtube("Code with Rupesh", 0);
+
+        Scanner sc = new Scanner(System.in);
+
+        // Create object with PIN = 1234
+        Youtube y1 = new Youtube("Code with Rupesh", 5, 1234);
+
         y1.getChannelName();
-        y1.getSubscriberCount();
-        y1.increaseSubscriberCount();
-        y1.getSubscriberCount();
-        y1.decreaseSubscriberCount();
-        y1.getSubscriberCount();
-        y1.increaseSubscriberCount();
-        y1.getSubscriberCount();
-        y1.increaseSubscriberCount();
-        y1.getSubscriberCount();
-        y1.increaseSubscriberCount();
-        y1.getSubscriberCount();
-        y1.increaseSubscriberCount();
-        y1.getSubscriberCount();
-        y1.increaseSubscriberCount();
-        y1.getSubscriberCount();
-        y1.increaseSubscriberCount();
-        y1.getSubscriberCount();
-        y1.decreaseSubscriberCount();
-        y1.getSubscriberCount();
-        y1.increaseSubscriberCount();
-        y1.getSubscriberCount();
-        y1.increaseSubscriberCount();
-        y1.getSubscriberCount();
+
+        // Ask user to enter PIN
+        System.out.print("Enter PIN to view subscriber count: ");
+        int userPin = sc.nextInt();
+
+        // Check PIN
+        y1.getSubscriberCount(userPin);
+
+        sc.close();
     }
 }
