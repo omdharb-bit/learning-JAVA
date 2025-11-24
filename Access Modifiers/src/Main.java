55d5231041db8b0ba2361ffce49044b92e0f376c
Om Dhar Badgaiyan <omdharb@gmail.com>
2025-11-24 11:52:33 +0530

    hii

--- Access Modifiers/src/Main.java
+++ Access Modifiers/src/Main.java
@@ -1,8 +1,12 @@
- import p1.a;
+import p1.a;
 
-public class Main {
-    public static void main(String[] args){
-        a a1=new a();
+class Main {
+    public static void main(String[] args) {
+        a a1 = new a();
         a1.show();
+        a1.publicVar();
+        a1.protectVar();
+        a1.defaultVar();
+        a1.privateVar();
     }
 }
--- Access Modifiers/src/p1/a.java
+++ Access Modifiers/src/p1/a.java
@@ -1,7 +1,32 @@
 package p1;
 
+
+
 public class a {
-    public void show(){
-        System.out.println("Hyy");
+
+    public int publicVar = 10;
+    protected int protectedVar = 20;
+    int defaultVar = 30;          // default access
+    private int privateVar = 40;
+
+    public void show() {
+        System.out.println("Inside class A:");
+
+    }
+
+    public void publicVar() {
+        System.out.println("publicVar: " + publicVar);
+    }
+
+    public void protectVar() {
+        System.out.println("protectedVar: " + protectedVar);
+    }
+
+    public void defaultVar() {
+        System.out.println("defaultVar: " + defaultVar);
+    }
+
+    public void privateVar() {
+        System.out.println("privateVar: " + privateVar);
     }
 }
