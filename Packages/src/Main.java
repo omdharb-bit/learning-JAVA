0314335f82d0be9381484f1c6b977dc2d435f9c6
Om Dhar Badgaiyan <omdharb@gmail.com>
2025-11-24 11:08:55 +0530

    hii

---
+++ Packages/src/Main.java
@@ -0,0 +1,14 @@
+ import p1.a;
+import p1.b;
+import p2.c;
+
+public class Main {
+public static void main(String[] args){
+    a a1=new a();
+    b b1=new b();
+    c c1=new c();
+    a1.show();
+    b1.show();
+c1.show();
+}
+}
