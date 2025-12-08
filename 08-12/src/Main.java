f8db8dbcacaff57b987bc2bb20c091e0ee975e8e
Om Dhar Badgaiyan <omdharb@gmail.com>
2025-12-08 15:43:59 +0530

    hii

--- 08-12/src/Main.java
+++ 08-12/src/Main.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.ListIterator;
 
 class Main {
@@ -10,16 +9,28 @@ class Main {
         list.add(3);
         list.add(4);
         list.add(5);
+        list.add(6);
+        list.add(7);
+        list.add(8);
+        list.add(9);
 
-        // Using ListIterator to update elements
+        int st = 0;
+        int end = list.size() - 1;
+        int mid = st + (end - st) / 2;
+
         ListIterator<Integer> it = list.listIterator();
+        int index = 0;
 
-        while (it.hasNext()) {
+        while (it.hasNext() && it.nextIndex() <= mid) {
             int val = it.next();
-            if (val == 4) {
-                it.set(10);   // replace 4 with 10
-            }
+            System.out.println(val);
         }
+
+        while(it.hasPrevious()){
+            int val1 =  it.previous();
+            System.out.println(val1);
+        }
+
         System.out.println("After modification: " + list);
     }
 }
