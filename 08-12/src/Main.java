c0ab6a22c9a3e158ff43fdee409eebc5be1e1c55
Om Dhar Badgaiyan <omdharb@gmail.com>
2025-12-08 14:17:39 +0530

    hii

---
+++ 08-12/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        ArrayList<Integer>list=new ArrayList<>();
+        list.add(5);
+        list.add(6);
+        list.add(7);
+        Iterator<Integer> it=list.iterator();
+        while(it.hasNext()){
+            int val= it.next();
+//            System.out.println(it.next());
+            if(val == 6){
+
+                it.remove();
+
+            }
+        }
+
+        System.out.println("After removal: "+list);
+    }
+
+
+}
---
+++ data sorting/src/Main.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+//class Main{
+//    public static void main(String[] args){
+//        ArrayList<Integer> list=new ArrayList<>();
+//        list.add(5);
+//        list.add(3);
+//System.out.println(list);
+//        Collections.sort(list);
+//        System.out.println(list);
+//    }
+// }
+
+
+//class student implements  Comparable<student> {
+//
+//    int RollNo;
+//    String Name;
+//    public student(int RollNo, String Name){
+//        this.RollNo=RollNo;
+//        this.Name=Name;
+//    }
+//    public int compareTo(student o){
+//        return  this.RollNo-o.RollNo;
+//
+//    }
+//    public String toString(){
+//        return "Student RollNo: "+RollNo+" ,Name:" +Name;
+//    }
+//}
+//
+//
+//class Main{
+//    public static void main(String[] args) {
+//        ArrayList<student>list=new ArrayList<>();
+//        list.add(new student(216,"OM"));
+//        list.add(new student(139,"Hittu"));
+//
+//        Collections.sort(list);
+//        System.out.println(list);
+//    }
+//}
+
